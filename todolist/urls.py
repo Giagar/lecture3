b050fe2f3d98d69aa26da7c540d3c3858b2c582d
Giga <23157357+Giagar@users.noreply.github.com>
2020-09-26 19:48:47 +0200

    done template inheritance; practice_template working correctly

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("practice_loop", views.practice_loop, name="practice_loop")
+    path("practice_loop", views.practice_loop, name="practice_loop"),
+    path("practice_template_page1", views.practice_template_page1, name="practice_template_page1"),
+    path("practice_template_page2", views.practice_template_page2, name="practice_template_page2"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -7,3 +7,10 @@ def practice_loop(request):
     return render(request, "todolist/practice_loop.html", {
         "tasks": tasks
     })
+
+def practice_template_page1(request):
+    return render(request, "todolist/practice_template_page1.html")
+
+
+def practice_template_page2(request):
+    return render(request, "todolist/practice_template_page2.html")

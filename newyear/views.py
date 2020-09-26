cc961eae1e4140ea7abd4ddd61fb888edc710a25
Giga <23157357+Giagar@users.noreply.github.com>
2020-09-26 13:17:46 +0200

    first commit

---
+++ newyear/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+def index(request):
+    today = datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": today.month == 1 and today.day == 1,
+    })

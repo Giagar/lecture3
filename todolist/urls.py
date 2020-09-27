1e789ec3b7aedd8312fdb601752186ed4c128715
Giga <23157357+Giagar@users.noreply.github.com>
2020-09-27 13:02:53 +0200

    add links to both page1 and page2

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "todolist"
 urlpatterns = [
     path("practice_loop", views.practice_loop, name="practice_loop"),
     path("practice_template_page1", views.practice_template_page1, name="practice_template_page1"),

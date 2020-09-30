bce18ee8fd9d43d672584859f9dda4fbe04415b1
Giga <23157357+Giagar@users.noreply.github.com>
2020-09-30 13:08:45 +0200

    all done but style in static; everything working

--- listtodo/views.py
+++ listtodo/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django import forms
+
+class NewForm(forms.Form):
+    todo = forms.CharField(label="New Task", min_length=2)
 
 # Create your views here.
 def index(request):
-    return render(request, "listtodo/index.html")
+    if "todos" not in request.session:
+        request.session["todos"] = []
+    return render(request, "listtodo/index.html", {
+        "todos": request.session["todos"]
+    })
 
 def add_task(request):
-    return render(request, "listtodo/add_task.html")
+    if request.method == "POST":
+        form = NewForm((request.POST))
+        if form.is_valid():
+            todo = form.cleaned_data["todo"]
+            request.session["todos"] += [todo]
+            return HttpResponseRedirect(reverse("listtodo:index"))
+        else:
+            return render(request, "listtodo/add_task.html", {
+                "form": form
+                })
+
+    return render(request, "listtodo/add_task.html", {
+        "form": NewForm(),
+    })
